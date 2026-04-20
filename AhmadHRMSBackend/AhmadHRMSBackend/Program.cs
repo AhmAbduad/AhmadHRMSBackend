05235ea7c7673ea56ab941d8557c72a4514b364e
Ahmad Abdullah <l190991@lhr.nu.edu.pk>
2026-04-20 18:09:08 +0500

    update program.cs file

--- AhmadHRMSBackend/AhmadHRMSBackend/Program.cs
+++ AhmadHRMSBackend/AhmadHRMSBackend/Program.cs
@@ -36,7 +36,10 @@ builder.Services.AddSwaggerGen();
 
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseMySql(
+    builder.Configuration.GetConnectionString("DefaultConnection"),
+    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+));
 
 
 builder.Services.AddScoped<IEmployeeList, EmployeeListRepository>();
