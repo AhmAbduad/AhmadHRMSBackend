85ecbae9c564d0f40b139839831177ff0a106b6b
Ahmad Abdullah <l190991@lhr.nu.edu.pk>
2026-06-18 18:47:19 +0500

    update program.cs file

--- AhmadHRMSBackend/AhmadHRMSBackend/Program.cs
+++ AhmadHRMSBackend/AhmadHRMSBackend/Program.cs
@@ -36,7 +36,7 @@ builder.Services.AddSwaggerGen();
 
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-  options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+  options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 builder.Services.AddScoped<IEmployeeList, EmployeeListRepository>();
